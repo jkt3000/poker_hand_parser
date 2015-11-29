124e23c8afb6dcefa81ad9eca4e9b88cfa11da46
John Tajima <manjiro@gmail.com>
2015-11-28 20:57:25 -0500

    wip parser actions

--- lib/poker_hand_parser.rb
+++ lib/poker_hand_parser.rb
@@ -1,11 +1,34 @@
 require 'json'
 require 'active_support'
 require "poker_hand_parser/version"
-
+require 'poker_hand_parser/game'
+require 'poker_hand_parser/table'
+require 'poker_hand_parser/player'
+require "poker_hand_parser/pokerstars/parser"
 
 module PokerHandParser
 
   extend self
+
+  def import_from_file(filehandle)
+    # handles splitting file into games
+    # determine which parser to use
+    # for each game history,
+      # parse history
+    # builds hash of parsed hands, # failed
+  end
+
+  def game_histories
+
+  end
+
+  def parse_game(game)
+    # returns json of parsed game and error message
+    # if successful,
+      # returns <json>, status: ok
+    # if failed
+      # returns nil, status: failed, message: .....
+  end
 
   # Your code goes here...
   def import(file)
